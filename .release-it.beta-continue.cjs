addf04a9c2658af1cce6c0f370b0241b9a350065
Quorafind <bonopengate@gmail.com>
2025-08-27 17:45:19 +0800

    chore: update workflow scripts and fix github tag issues

---
+++ .release-it.beta-continue.cjs
@@ -0,0 +1,9 @@
+// This config is specifically for continuing an existing beta sequence
+// It does NOT set preRelease to force a new identifier
+const baseConfig = require('./.release-it.beta.cjs');
+
+module.exports = {
+	...baseConfig,
+	// Remove the preRelease setting to allow continuing the existing sequence
+	preRelease: undefined
+};
